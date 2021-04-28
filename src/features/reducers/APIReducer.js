e75e3ec0618e784fb02f314032f83addacddf2ee
Tarafder, Tanvir (AU - Sydney) <tatarafder@deloitte.com.au>
2021-04-28 21:50:20 +1000

    adding build folder after running npm run build

--- src/features/reducers/APIReducer.js
+++ src/features/reducers/APIReducer.js
@@ -1,31 +1,3 @@
-/*import { useReducer } from 'react';
-import sampleData from '../../app/api/sampleData';
-import { CALL_YOUTUBE_API, UPDATE_URL } from '../actions/eventConstants';
-
-const initialState = {
-    youtubeVideo: {}
-}
-
-
-export default function APIReducer(state = initialState,  action ) {
-
-    console.log("reducer "+JSON.stringify(action.payload)+" state: "+JSON.stringify(state)+ "action type "+action.type)
-    switch(action.type) {
-        case CALL_YOUTUBE_API: 
-            return {
-                ...state,
-                youtubeVideo: action.payload
-            }
-        case UPDATE_URL: 
-            return {
-                ...state,
-                URL: action.payload
-            }
-        default: return state;
-    }
-
-}
-*/
 
 import { createSlice } from '@reduxjs/toolkit'
 
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux';
 import testReducer from '../features/sandbox/testReducer';
 import youtubevideo from '../features/reducers/APIReducer';
-import RedirectReducer from '../features/reducers/RedirectReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
